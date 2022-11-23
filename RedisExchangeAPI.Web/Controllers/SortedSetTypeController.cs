621310b02bbe586fe624cd716df6d60fa91693ac
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2022-11-23 18:01:35 +0300

    Redis SortedSet

---
+++ RedisExchangeAPI.Web/Controllers/SortedSetTypeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class SortedSetTypeController : Controller
+    {
+        private readonly IDatabase _redisDb;
+        private readonly string key = "sortedSetTypeNames";
+
+        public SortedSetTypeController(IDatabase redisDb)
+        {
+            _redisDb = redisDb;
+        }
+
+        public IActionResult Index()
+        {
+            HashSet<string> nameList = new();
+
+            if (_redisDb.KeyExists(key))
+            {
+                //nameList = _redisDb.SortedSetScan(key).Select(x => x.ToString()).ToHashSet();
+                nameList = _redisDb.SortedSetRangeByRank(key, order: Order.Descending).Select(x => x.ToString()).ToHashSet();
+            }
+
+            return View(nameList);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name, double score)
+        {
+            _redisDb.SortedSetAdd(key, name, score);
+            _redisDb.KeyExpire(key, DateTime.Now.AddMinutes(5));
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(string name)
+        {
+            _redisDb.SortedSetRemove(key, name);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
