e16a0f332fbb727f5506326f5b81bdb598d6c84b
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2022-11-24 12:52:05 +0300

    Revert

---
+++ RedisExampleApp.API/Repository/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using RedisExampleApp.API.Models;
+
+namespace RedisExampleApp.API.Repository
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> GetProductsAsync();
+        Task<Product> GetByIdAsync(int id);
+        Task<Product> AddAsync(Product product);
+        
+    }
+}
